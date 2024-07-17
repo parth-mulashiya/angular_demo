e2c6339f65bf87bb104f6a65552e2748fdc6bbf9
parth-mulashiya <parth1282m@gmail.com>
2024-07-17 12:40:31 +0530

    first commit

---
+++ src/app/components/about/about.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  standalone: true,
+  imports: [],
+  template: '<h1>About Component </h1>',
+  styles: [
+    'h1{color: red}'
+  ]
+})
+export class AboutComponent {
+
+}
