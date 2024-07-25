0d2a570e993d745d15d5abb9edfe454396896743
parth-mulashiya <parth1282m@gmail.com>
2024-07-25 17:07:27 +0530

    for loop added

---
+++ src/app/components/for-loop/for-loop.component.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-for-loop',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './for-loop.component.html',
+  styleUrl: './for-loop.component.css'
+})
+export class ForLoopComponent {
+
+  fruits = ['Apple', 'Banana', 'Orange']
+
+  i = 1
+}
