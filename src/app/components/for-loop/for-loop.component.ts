2f69ce49cc001e070a53ddf92786f7b3de1c6a3e
parth-mulashiya <parth1282m@gmail.com>
2024-07-30 14:18:53 +0530

    for loop new ex added

--- src/app/components/for-loop/for-loop.component.ts
+++ src/app/components/for-loop/for-loop.component.ts
@@ -12,4 +12,13 @@ export class ForLoopComponent {
 
   fruits = ['Apple', 'Banana', 'Orange']
 
+  employees = [
+    {id:101,name: "Parth",salary:30000,age:22},
+    {id:102,name: "Prashant",salary:24000,age:24},
+    {id:103,name: "Nirav",salary:27000,age:23},
+    {id:104,name: "Bapu",salary:33000,age:21},
+    {id:105,name: "Harsh",salary:38000,age:23},
+    {id:106,name: "Vijay",salary:21000,age:21},
+  ]
+
 }
