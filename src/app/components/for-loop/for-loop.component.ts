cf76c2ed283e4626e7d32a2a5ea05107ca70e1ad
parth-mulashiya <parth1282m@gmail.com>
2024-07-31 18:09:53 +0530

    for loop old syntax

--- src/app/components/for-loop/for-loop.component.ts
+++ src/app/components/for-loop/for-loop.component.ts
@@ -10,6 +10,9 @@ import { Component } from '@angular/core';
 })
 export class ForLoopComponent {
 
+
+  items = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
   fruits = ['Apple', 'Banana', 'Orange']
 
   employees:any = [
